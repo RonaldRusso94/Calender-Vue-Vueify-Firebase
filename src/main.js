96261634e9e1e900f7ebd8cbbad3c3487b2633d6
Ronald Russo <RonaldRusso94@yahoo.com>
2020-09-21 19:37:10 -0400

    Update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-Vue.config.productionTip = false
+Vue.use(VueTextareaAutosize);
+
+Vue.config.productionTip = false;
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyC5tqJ06QIWPDAHBlpndjygaT7NJU942ok',
+  authDomain: 'calender-vue-vueify-fire-1e159.firebaseapp.com',
+  databaseURL: 'https://calender-vue-vueify-fire-1e159.firebaseio.com',
+  projectId: 'calender-vue-vueify-fire-1e159',
+  storageBucket: 'calender-vue-vueify-fire-1e159.appspot.com',
+  messagingSenderId: '920187803700',
+  appId: '1:920187803700:web:c99084177a250e713a5261',
+});
+
+export const db = firebase.firestore();
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App),
+}).$mount('#app');
